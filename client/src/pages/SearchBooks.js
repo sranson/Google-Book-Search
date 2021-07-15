c7678de69003699d9a6d8ee10fb8868d70a61e3f
Salome Ranson <salomeranson@gmail.com>
2021-07-14 21:23:37 -0400

    unable to apply SAVE_BOOK mutation to front-end functionality

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
-
+import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { SAVE_BOOK } from '../utils/mutations';
 
 
 
@@ -72,8 +73,13 @@ const SearchBooks = () => {
       return false;
     }
 
+    const { loading, data } = useMutation(SAVE_BOOK);
+    const saveBook = data.savedBooks || [];
+
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await saveBook({
+        variables: { bookId }
+      });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
