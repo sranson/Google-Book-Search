ba9125680a49565c58cf293f65a8deabf0f33838
Salome Ranson <salomeranson@gmail.com>
2021-07-21 15:07:34 -0400

    adding context for authentication

--- server/server.js
+++ server/server.js
@@ -1,15 +1,23 @@
 const express = require('express');
-const path = require('path');
+
+
+
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+const { authMiddleware } = require("./utils/auth")
+
 const { typeDefs, resolvers } = require('./schemas');
+
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: authMiddleware
 });
 
 
